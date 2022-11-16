ebb06fc3dadc490938adeb5c2ef6f939dff76f72
juanarancibia <arancibia.juan22@gmail.com>
2022-11-16 10:44:38 -0300

    Add autorotate

--- threejs/camera-controller.tsx
+++ threejs/camera-controller.tsx
@@ -1,4 +1,4 @@
-import { Camera, useThree } from "@react-three/fiber";
+import { Camera, useFrame, useThree } from "@react-three/fiber";
 import { useEffect } from "react";
 import { PerspectiveCamera, Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -7,13 +7,17 @@ const CameraController = () => {
   const { camera, gl } = useThree();
   (camera as PerspectiveCamera).fov = 50;
   (camera as PerspectiveCamera).zoom = 1.2;
+  let controls: OrbitControls;
 
   useEffect(() => {
-    const controls = new OrbitControls(camera, gl.domElement);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    controls = new OrbitControls(camera, gl.domElement);
     controls.target = new Vector3(0, 1, 0);
     controls.maxPolarAngle = Math.PI / 2;
     controls.minPolarAngle = Math.PI / 2;
     controls.rotateSpeed = 0.2;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 0.5;
     controls.enableZoom = true;
     controls.maxDistance = 8;
     controls.enablePan = false;
@@ -23,6 +27,8 @@ const CameraController = () => {
       controls.dispose();
     };
   }, [camera, gl]);
+
+  useFrame(() => controls.update());
 
   return null;
 };
